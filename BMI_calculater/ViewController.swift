17146b6abeb37025ea6ea4dbc4ed9850d78eeb3c
林宜潔 <>
2018-12-03 20:56:09 +0800

    init

---
+++ BMI_calculater/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  BMI_calculater
+//
+//  Created by 林宜潔 on 2018/12/3.
+//  Copyright © 2018年 林宜潔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+// 類似Java 的Activity
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func heightText(_ sender: UITextField) {
+    }
+    @IBAction func weightText(_ sender: UITextField) {
+    }
+    @IBAction func okBtn(_ sender: UIButton) {
+    }
+    @IBAction func resetBtn(_ sender: UIButton) {
+    }
+    @IBOutlet weak var bmiLabel: UILabel!
+}
+
